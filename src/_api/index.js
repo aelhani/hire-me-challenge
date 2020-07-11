ab329f0fc065a70202a69243f6f8d98b222fde0b
Abderrazak <abderrazak.elhani@gmail.com>
2020-07-11 22:36:02 +0100

    rectify request function name

--- src/_api/index.js
+++ src/_api/index.js
@@ -1,6 +1,6 @@
 import request from './request'
 
-export async function loadPokemons(limit, offset) {
+export async function loadPokemonsRequest(limit, offset) {
     return await request.get(`/pokemon?limit=${limit}&offset=${offset}`)
         .then(res => {
             return res
