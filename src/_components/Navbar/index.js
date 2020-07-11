07b5d91eea659a599e43167208bc975c5d5fcf5d
Abderrazak <abderrazak.elhani@gmail.com>
2020-07-11 18:42:49 +0100

    add Drawer to Navbar

--- src/_components/Navbar/index.js
+++ src/_components/Navbar/index.js
@@ -7,6 +7,8 @@ import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 
+import Drawer from './Drawer';
+
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
@@ -26,9 +28,7 @@ export default function ButtonAppBar() {
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-            <MenuIcon />
-          </IconButton>
+          <Drawer />
           <Typography variant="h6" className={classes.title}>
             News
           </Typography>
