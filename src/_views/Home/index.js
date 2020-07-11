de2b715b75a80d43cce4ebe0bdf64568cb34125a
Abderrazak <abderrazak.elhani@gmail.com>
2020-07-11 23:37:56 +0100

    load pokemons on button click for testing purposes

--- src/_views/Home/index.js
+++ src/_views/Home/index.js
@@ -1,11 +1,32 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import Button from '@material-ui/core/Button'
+import { loadPokemons } from '../../_actions'
 
-const Home = () => {
+const Home = (props) => {
+
+    console.log(props.pokemons)
+
     return (
         <div>
-            Home
+            <Button
+                // temporary button for testing purposes
+                color="primary"
+                variant="contained"
+                onClick={() => props.loadPokemons(20, 0)}
+            >
+                Load
+            </Button>
         </div>
     )
 };
 
-export default Home;
+const mapState = (state) => {
+    console.log(state)
+    return {
+        pokemons: state.pokemonsReducer.data,
+        message: state.pokemonsReducer.message,
+    }
+}
+
+export default connect(mapState, { loadPokemons })(Home)
