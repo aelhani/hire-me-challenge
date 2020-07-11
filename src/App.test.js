e5cd442ac22ac775808e0575daccb4fb43fc2102
Abderrazak Elhani <abderrazak.elhani@hps-worldwide.com>
2020-07-11 16:50:50 +0100

    add test for App render

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+it('renders App global container', () => {
+  const { getByTitle } = render(<App />);
+  const element = getByTitle("app-container");
+  expect(element).toBeInTheDocument();
 });
