4418ef657cf933141a57cb1608e08d4483560389
Abderrazak Elhani <abderrazak.elhani@hps-worldwide.com>
2020-07-11 17:49:22 +0100

    setup Navbar and Routes in App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import Navbar from './_components/Navbar';
+
+import Home from './_views/Home';
+import Types from './_views/Types';
+import Pokemon from './_views/Pokemon';
 
 const App = () => {
   return (
     <div className="App" title="app-container" >
+      <Navbar />
+      <Router>
+          <Route exact path="/" render={() => <Home />} />
+          <Route exact path="/types" render={() => <Types />} />
+          <Route exact path="/pokemon" render={() => <Pokemon />} />
+      </Router>
     </div>
   );
 }
