2925a2fe7479ca13f3c4d12ebd80cc29c3f7b160
Abderrazak <abderrazak.elhani@gmail.com>
2020-07-11 23:35:43 +0100

    apply middleware redux-thunk

--- src/index.js
+++ src/index.js
@@ -4,14 +4,25 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import thunk from 'redux-thunk';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import reducer from './_reducers/';
+
+
+const store = createStore(reducer, applyMiddleware(thunk));
+
+const WebApp = () => {
+  return (
+    <Provider store={store}>
+      <App />
+    </Provider>
+  )
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+ReactDOM.render(<WebApp />, document.getElementById('root'));
