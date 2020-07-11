5df76e6e0ab1b2ef2b42438fffa11c389ffdf010
Abderrazak <abderrazak.elhani@gmail.com>
2020-07-11 22:36:59 +0100

    define action loadPokemons

---
+++ src/_actions/dataActions.js
@@ -0,0 +1,11 @@
+import types from '../_types/'
+
+import { loadPokemonsRequest } from '../_api/'
+
+export const loadPokemons = (limit, offset) => async dispatch => {
+    const response = await loadPokemonsRequest(limit, offset)
+    if (response.status === 200)
+        dispatch({ type: types.LOAD_POKEMONS_SUCCESS, payload: response.data })
+    else
+        dispatch({ type: types.LOAD_POKEMONS_FAILURE, payload: response })
+}
