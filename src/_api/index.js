8ee360cdfba71a4eda6c43d71487edf6968c1ab8
Abderrazak <abderrazak.elhani@gmail.com>
2020-07-12 02:13:05 +0100

    add pokeItem api request

--- src/_api/index.js
+++ src/_api/index.js
@@ -1,7 +1,17 @@
 import request from './request'
 
-export async function loadPokemonsRequest(limit, offset) {
+export async function loadPokeListRequest(limit, offset) {
     return await request.get(`/pokemon?limit=${limit}&offset=${offset}`)
+        .then(res => {
+            return res
+        })
+        .catch(error => {
+            return error.response.data
+        })
+}
+
+export async function loadPokeItemRequest(id) {
+    return await request.get(`/pokemon/${id}`)
         .then(res => {
             return res
         })
