6b1a7df1e63e265cfd2263272f5618f4e4a74e34
Abderrazak <abderrazak.elhani@gmail.com>
2020-07-12 03:45:07 +0100

    add lisk to pokeItem route

--- src/_components/PokeList/index.js
+++ src/_components/PokeList/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Link from '@material-ui/core/Link';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
@@ -6,19 +8,32 @@ import ListItemText from '@material-ui/core/ListItemText';
 import Divider from '@material-ui/core/Divider';
 import AdbIcon from '@material-ui/icons/Adb';
 
+const useStyles = makeStyles({
+    link: {
+        color: '#222',
+        textDecoration: 'none',
+    }
+});
+
 const PokeList = ({ list }) => {
+    const classes = useStyles();
 
     return (
         <List component="nav" >
             {list !== undefined && list.map((pokemon, i) => {
                 return (
                     <React.Fragment key={`${i}-${pokemon.name}`} >
-                        <ListItem button>
-                            <ListItemIcon>
-                                <AdbIcon />
-                            </ListItemIcon>
-                            <ListItemText primary={pokemon.name} />
-                        </ListItem>
+                        <Link
+                            href={`/pokemon/${i + 1}`}
+                            className={classes.link}
+                        >
+                            <ListItem button >
+                                <ListItemIcon>
+                                    <AdbIcon />
+                                </ListItemIcon>
+                                <ListItemText primary={pokemon.name} />
+                            </ListItem>
+                        </Link>
                         <Divider />
                     </React.Fragment>
                 )
