2f2fb248585b67043e8ad59fea2f56b9168f1a46
Abderrazak <abderrazak.elhani@gmail.com>
2020-07-13 02:57:45 +0100

    add load by type param processing in home page

--- src/_views/Home/index.js
+++ src/_views/Home/index.js
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button'
 import Typography from '@material-ui/core/Typography';
-import { loadPokeList } from '../../_actions'
+import { loadPokeList, loadPokeListByType } from '../../_actions'
 
 import PokeList from '../../_components/PokeList';
 
@@ -32,10 +32,25 @@ const useStyles = makeStyles((theme) => ({
 const Home = (props) => {
     const classes = useStyles();
 
+    function getType() {
+        let url_string = window.location.href
+        let url = new URL(url_string);
+        let type = url.searchParams.get("type");
+        return type;
+    }
+
     const [page, setPage] = useState(0);
+    const [byType, setByType] = useState(false);
 
     useEffect(() => {
-        props.loadPokeList(20, page)
+        if (getType() !== null && parseInt(getType()) > 0) {
+            props.loadPokeListByType(getType())
+            setByType(true);
+        }
+        else {
+            props.loadPokeList(20, page)
+            setByType(false);
+        }
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [page]);
 
@@ -67,7 +82,7 @@ const Home = (props) => {
                     {">"}
                 </Button>
             </div>
-            {props.pokeList !== null && <PokeList list={props.pokeList.results} />}
+            {props.pokeList !== null && <PokeList byType={byType} list={props.pokeList} />}
         </div>
     )
 };
@@ -79,4 +94,4 @@ const mapState = (state) => {
     }
 }
 
-export default connect(mapState, { loadPokeList })(Home)
+export default connect(mapState, { loadPokeList, loadPokeListByType })(Home)
