5625da9b7add77dd7f60f40dbe8763f81ae8a715
Abderrazak <abderrazak.elhani@gmail.com>
2020-07-17 19:17:06 +0100

    Updates

---
+++ precache-manifest.aa6e41cd6931e1acf95f6c86bd246cd8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b9123d65c68f6f4b57f42b0343e19c6",
+    "url": "/hire-me-challenge/index.html"
+  },
+  {
+    "revision": "7ddb038539b1a0e53417",
+    "url": "/hire-me-challenge/static/css/main.5dc1c6f9.chunk.css"
+  },
+  {
+    "revision": "1daafdb2abb5cefe2416",
+    "url": "/hire-me-challenge/static/js/2.9d3c347e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/hire-me-challenge/static/js/2.9d3c347e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ddb038539b1a0e53417",
+    "url": "/hire-me-challenge/static/js/main.7ae38885.chunk.js"
+  },
+  {
+    "revision": "e02a945b4cae7b7b1e8a",
+    "url": "/hire-me-challenge/static/js/runtime-main.2b87ede9.js"
+  }
+]);
